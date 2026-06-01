0ac638c99136d10cfa18b3681d7f5ee82645419b
YaGrease <spackman.spiderman@gmail.com>
2026-06-01 16:10:54 -0600

    updated method to pull information with .bat file

---
+++ backupToGit.bat
@@ -0,0 +1,16 @@
+:: Robot FTP Backup Script
+:: Downloads all .ls program files from FANUC controller
+:: Robot IP: 192.168.1.101
+:: Credentials: fanuc/fanuc
+:: Output folder: C:\robot_backup
+
+echo bin> %TEMP%\ftpcmds.txt
+echo prompt>> %TEMP%\ftpcmds.txt
+echo lcd C:\robot_backup>> %TEMP%\ftpcmds.txt
+echo cd /FR/tp>> %TEMP%\ftpcmds.txt
+echo mget *.ls>> %TEMP%\ftpcmds.txt
+echo quit>> %TEMP%\ftpcmds.txt
+
+ftp -s:%TEMP%\ftpcmds.txt 192.168.1.101
+
+del %TEMP%\ftpcmds.txt
